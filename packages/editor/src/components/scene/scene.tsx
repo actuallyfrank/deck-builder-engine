9360d95a51bfe8dd21d069fef24548eb37b2d8cb
actuallyfrank <7669295+actuallyfrank@users.noreply.github.com>
2025-03-24 08:24:54 +0100

    style: improve item selection

--- packages/editor/src/components/scene/scene.tsx
+++ packages/editor/src/components/scene/scene.tsx
@@ -1,5 +1,6 @@
 import { Panel } from "../panel/panel";
 import { useScene } from "../../provider/scene-provider";
+import "./scene.css";
 
 export const Scene = () => {
   const { sceneItems, selectItem, selectedItemId } = useScene();
@@ -7,8 +8,13 @@ export const Scene = () => {
   return (
     <Panel name="scene">
       {sceneItems.map((item) => (
-        <div role="button" key={item.id} onClick={() => selectItem(item.id)}>
-          {item.name} {selectedItemId === item.id ? "selected" : ""}
+        <div
+          className={`item ${selectedItemId === item.id ? "item-selected" : ""}`}
+          role="button"
+          key={item.id}
+          onClick={() => selectItem(item.id)}
+        >
+          {item.name}
         </div>
       ))}
     </Panel>
