4f58125af35ea5b8398acb00d88ee84a3bf6c9f7
actuallyfrank <7669295+actuallyfrank@users.noreply.github.com>
2025-03-29 10:25:24 +0100

    snap: canvas gets added multiple times

--- packages/editor/src/components/viewport/viewport.tsx
+++ packages/editor/src/components/viewport/viewport.tsx
@@ -1,33 +1,45 @@
 import { App, PIXI } from "core";
 import { Panel } from "../panel/panel";
-import { RefObject, useEffect, useRef } from "react";
+import { memo, RefObject, useEffect, useRef, useState } from "react";
+import { useScene } from "../../provider/scene-provider";
 
-export const Viewport = () => {
+export const Viewport = memo(() => {
   const containerRef = useRef<null | HTMLDivElement>(null);
   return (
     <Panel ref={containerRef} name="viewport">
       {containerRef && <RenderViewport containerRef={containerRef} />}
     </Panel>
   );
-};
+});
 
 export const RenderViewport = ({
   containerRef,
 }: {
   containerRef: RefObject<HTMLDivElement | null>;
 }) => {
+  const { sceneItems } = useScene();
+  const appRef = useRef<PIXI.Application | null>(null);
+
   useEffect(() => {
+    if (appRef.current) {
+      return;
+    }
     const initializeApp = async () => {
       const app = await App(containerRef.current!);
-      await PIXI.Assets.load("sample.png");
 
       document.getElementById("viewport")?.appendChild(app.canvas);
 
-      app.stage.addChild(PIXI.Sprite.from("sample.png"));
+      appRef.current = app;
     };
 
     initializeApp();
   }, [containerRef]);
+
+  const graphics = new PIXI.Graphics();
+  graphics.beginFill(0xff0000);
+  graphics.drawRect(50, 50, 100, 100);
+  graphics.endFill();
+  appRef.current?.stage.addChild(graphics);
 
   return (
     <div
